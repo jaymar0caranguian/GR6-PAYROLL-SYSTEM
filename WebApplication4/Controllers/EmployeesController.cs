5afe35bad9828ba92cfca562ce0598f927bd126c
Jaymar Caranguian <145180662+jaymar0caranguian@users.noreply.github.com>
2023-12-17 19:43:53 +0800

    migrate all payroll files from old repository

---
+++ WebApplication4/Controllers/EmployeesController.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApplication4.DataDB;
+
+namespace WebApplication4.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly PmsDatabaseContext _context;
+
+        public EmployeesController(PmsDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Employees
+        public async Task<IActionResult> Index(int page = 1, int pageSize = 5)
+        {
+            // Calculate total number of pages
+            var totalCount = await _context.Employee.CountAsync();
+            var totalPages = (int)Math.Ceiling((double)totalCount / pageSize);
+
+            // Validate page number
+            if (page < 1)
+            {
+                page = 1;
+            }
+            else if (page > totalPages)
+            {
+                page = totalPages;
+            }
+
+            // Retrieve employees for the current page
+            var employees = await _context.Employee
+                .OrderBy(e => e.EmpId)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            // Set ViewBag values for use in the view
+            ViewBag.TotalPages = totalPages;
+            ViewBag.CurrentPage = page;
+
+            return View(employees);
+        }
+
+        // GET: Employees/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Employee == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employee
+                .FirstOrDefaultAsync(m => m.EmpId == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        // GET: Employees/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employees/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("EmpId,Fname,Mname,Lname,Position,Rate,SssNo,PagibigNo")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(employee);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employee);
+        }
+
+        // GET: Employees/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Employee == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employee.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employees/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("EmpId,Fname,Mname,Lname,Position,Rate,SssNo,PagibigNo")] Employee employee)
+        {
+            if (id != employee.EmpId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(employee);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EmployeeExists(employee.EmpId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employee);
+        }
+
+        // GET: Employees/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Employee == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employee
+                .FirstOrDefaultAsync(m => m.EmpId == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Employee == null)
+            {
+                return Problem("Entity set 'PmsDatabaseContext.Employee'  is null.");
+            }
+            var employee = await _context.Employee.FindAsync(id);
+            if (employee != null)
+            {
+                _context.Employee.Remove(employee);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EmployeeExists(int id)
+        {
+          return (_context.Employee?.Any(e => e.EmpId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ WebApplication4/Controllers/PagesController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication4.DataDB;
+
+namespace WebApplication4.Controllers
+{
+    public class PagesController : Controller
+    {
+		private readonly PmsDatabaseContext _context;
+
+		public PagesController(PmsDatabaseContext context)
+		{
+			_context = context;
+		}
+		public IActionResult Dashboard()
+        {
+            return View();
+        }
+        public IActionResult Attendance()
+        {
+            return View();
+        }
+        public IActionResult Deductions()
+        {
+            return View();
+        }
+        public IActionResult Employees()
+        {
+            return View();
+        }
+        public IActionResult ManageUsers()
+        {
+            return View();
+        }
+        public IActionResult Payslip()
+        {
+            var random = new Random();
+
+            var groupedQuery = from employee in _context.Employee
+                               join attendance in _context.Attds on employee.EmpId equals attendance.EmpId
+                               group new { employee, attendance } by employee.EmpId into grouped
+                               select new
+                               {
+                                   EmployeeId = grouped.Key,
+                                   Attendances = grouped.Select(a => new
+                                   {
+                                       Employee = a.employee,
+                                       Attendance = a.attendance
+                                   })
+                               };
+
+            var result = groupedQuery.AsEnumerable()
+                .Select(grouped => new
+                {
+                    EmployeeId = grouped.EmployeeId,
+                    EmployeeName = $"{grouped.Attendances.First().Employee.Fname} {grouped.Attendances.First().Employee.Mname} {grouped.Attendances.First().Employee.Lname}",
+                    FirstAttendanceDate = grouped.Attendances.OrderBy(a => a.Attendance.Date).FirstOrDefault()?.Attendance.Date,
+                    LatestAttendanceDate = grouped.Attendances.OrderByDescending(a => a.Attendance.Date).FirstOrDefault()?.Attendance.Date,
+                    TotalNdValue = grouped.Attendances.Sum(a => a.Attendance.nd),
+                    Rate = grouped.Attendances.First().Employee.Rate,
+                    TotalSalary = grouped.Attendances.Sum(a => a.Attendance.nd) * grouped.Attendances.First().Employee.Rate,
+
+                    RandomNumber = random.Next(1000, 9999)
+                })
+                .FirstOrDefault();
+
+            return View(result);
+        }
+
+        public IActionResult Positions()
+        {
+            return View();
+        }
+        public IActionResult Reports()
+        {
+            return View();
+        }
+    }
+}
